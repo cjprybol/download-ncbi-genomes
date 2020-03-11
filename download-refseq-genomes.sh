8e911641f50d198c2dd703f3ac4eff15e87852e0
Cameron Prybol <cameron.prybol@gmail.com>
2020-03-11 18:45:06 +0000

    Add notebook

--- download-refseq-genomes.sh
+++
@@ -1,5 +0,0 @@
-wget --mirror --no-directories https://ftp.ncbi.nlm.nih.gov/genomes/refseq/assembly_summary_refseq.txt
-grep -v "#" assembly_summary_refseq.txt \
-    | awk -F "\t" '{print $20}' \
-    | awk 'BEGIN{FS=OFS="/";filesuffix="genomic.fna.gz"}{ftpdir=$0;asm=$10;file=asm"_"filesuffix;print ftpdir,file}' \
-    | wget --mirror --no-directories --input-file -
